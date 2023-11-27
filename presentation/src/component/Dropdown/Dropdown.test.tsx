45ba66b6f11f15c00a935d63a1896ff5500b2cef
Aman404 <amanupadhyay404@gmail.com>
2023-11-27 13:21:20 +0530

    ..

--- presentation/src/component/Dropdown/Dropdown.test.tsx
+++ presentation/src/component/Dropdown/Dropdown.test.tsx
@@ -16,23 +16,23 @@ describe('Dropdown', () => {
 
   it('renders the dropdown with options', () => {
     render(<Dropdown options={options} />);
-    
+
     // Check if the options are rendered
     options.forEach((option) => {
       const optionElement = screen.getByText(option.value);
       expect(optionElement).toBeInTheDocument();
     });
-    
+
   });
- 
- 
+
+
 
   it('calls the onChange function when the dropdown value changes', () => {
     const onChangeMock = jest.fn();
     const options = [
-        { id: '1', value: 'Option 1' },
-        { id: '2', value: 'Option 2' },
-        { id: '3', value: 'Option 3' },
+      { id: '1', value: 'Option 1' },
+      { id: '2', value: 'Option 2' },
+      { id: '3', value: 'Option 3' },
     ];
     const { getByTestId } = render(<Dropdown options={options} onChange={onChangeMock} />);
     const dropdownElement = getByTestId('dropdown') as HTMLSelectElement;
@@ -41,27 +41,28 @@ describe('Dropdown', () => {
 
     expect(onChangeMock).toHaveBeenCalledTimes(1);
     expect(dropdownElement.value).toBe('2');
-});
-
-it('renders options correctly', () => {
-  const { getByTestId, getByText } = render(<Dropdown options={options} />);
-  const dropdown = getByTestId('dropdown');
-
-  expect(dropdown).toBeInTheDocument();
-
-  options.forEach((option) => {
-    const optionElement = getByText(option.value);
-    expect(optionElement).toBeInTheDocument();
-    expect(optionElement).toHaveValue(option.id);
-
-    if (option.isDisabled) {
-      expect(optionElement).toBeDisabled();
-    } else {
-      expect(optionElement).not.toBeDisabled();
-    }
   });
-});
+
+  it('renders disabled option correctly', () => {
+
+    const { getByTestId, getByText } = render(<Dropdown options={options} />);
+    const dropdown = getByTestId('dropdown');
+
+    expect(dropdown).toBeInTheDocument();
+
+    options.forEach((option) => {
+      const optionElement = getByText(option.value);
+      expect(optionElement).toBeInTheDocument();
+      expect(optionElement).toHaveValue(option.id);
+
+      if (option.isDisabled) {
+        expect(optionElement).toBeDisabled();
+      } else {
+        expect(optionElement).not.toBeDisabled();
+      }
+    });
+  });
 
 
- 
+
 });
